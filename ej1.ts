ae6805fa5adca0c74a410650ecf12825cfb93c88
matu-8 <agueromatias214@gmail.com>
2026-08-11 13:33:13 -0300

    Inicio de trabajo 1 - typescript

---
+++ ej1.ts
@@ -0,0 +1,14 @@
+//abstraccion ejercicio 1
+class Producto {
+    nombre: String
+    precio: Number
+    categoria: String
+    stock: Number
+  constructor(nombre: String, precio: Number, categoria: String, stock: Number){
+  this.nombre = nombre;
+  this.precio = precio;
+  this.categoria = categoria;
+  this.stock = stock;
+  }
+  
+}
