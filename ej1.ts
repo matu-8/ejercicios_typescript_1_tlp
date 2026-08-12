5e280fc6ebbd4698ffea86fe40063c14041a057f
matu-8 <agueromatias214@gmail.com>
2026-08-11 21:05:33 -0300

    ejercicio 1 abstraccion realizado

--- ej1.ts
+++ ej1.ts
@@ -1,14 +1,57 @@
 //abstraccion ejercicio 1
+
+const enum categoria {
+  ALMACEN = 'almacen',
+  ELECTRONICA = 'electronica',
+  LIMPIEZA = 'limpieza',
+  HOGAR = 'hogar'
+}
+
 class Producto {
     nombre: String
-    precio: Number
+    precio: number
     categoria: String
-    stock: Number
-  constructor(nombre: String, precio: Number, categoria: String, stock: Number){
+    stock: number
+  constructor(nombre: string, precio: number, categoria: string, stock: number){
   this.nombre = nombre;
   this.precio = precio;
   this.categoria = categoria;
   this.stock = stock;
   }
+  descrcibir(): String {
+    return `Producto: ${this.nombre} (${this.categoria}): $${this.precio} - ${this.stock} unidades.`
+  }
   
+  hayStock(cantidad: number): boolean{
+   let esNegativo:boolean = cantidad > 0 ? true : false
+   if(esNegativo){
+     console.log('Stock disponible')
+    }  else {
+     console.log('Faltante de stock, reponer')
+   }
+   return esNegativo;
+  }
+  venderUnidades(cantidad: number){
+    //let aviso: string = this.hayStock(cantidad) ? 'Producto vendido' : 'No se ha realizado la venta'
+    if(this.hayStock(cantidad)){
+      let resta: number = cantidad - 1
+      console.log('Producto vendido,', `stock restante: ${resta}`)
+    } else {
+      console.log('No se ha realizado la venta')
+    }
+  }
+  aplicarDescuento(porcentaje: number): number{
+    let numParaCalculo = porcentaje / 100;
+    let descAplicado = this.precio * numParaCalculo;
+    let precioFinal = this.precio - descAplicado; 
+    return precioFinal;
+  }
 }
+
+//instancio la clase 
+let leche = new Producto('Leche', 1800, categoria.ALMACEN, 10)
+
+console.log(leche.descrcibir())
+console.log(leche.hayStock(leche.stock))
+console.log(leche.venderUnidades(leche.stock))
+console.log('Precio final con descuento: ', leche.aplicarDescuento(10))
